1440fb1499a1292c9812cc89f2d4bf64fb4faceb
hanaharafumiki <hanahara@cornerprinting.com>
2020-02-18 12:53:54 +0900

    good関連を削除+itemsテーブルの作成

--- db/migrate/20200211115141_create_goods.rb
+++
@@ -1,8 +0,0 @@
-class CreateGoods < ActiveRecord::Migration[5.2]
-  def change
-    create_table :goods do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_114822) do
+ActiveRecord::Schema.define(version: 2020_02_18_034506) do
 
-  create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
