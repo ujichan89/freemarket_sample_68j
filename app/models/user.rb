ede5980ceb4e32d5ff6f09996d7cf28722cbb41f
reyon08 <sorairo.benimairoi@gmail.com>
2020-02-19 19:41:06 +0900

    [WIP]user登録
    #what
    new.scss
    user.scss
    user.rb
    new.html.haml
    new_address.html.haml
    index.html.haml
    上記のファイルに修正を加えました
    
    #why
    viewがずれてしまっていたため

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   validates :encrypted_password, presence: true, length: { minimum: 7 }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
+  # validates :family_name, presence: true, format: { with: /[\p{hiragana} ー－]+/ }
+  # validates :first_name, presence: true, format: { with: /[\p{hiragana} ー－]+/ }
   validates :family_furigana, presence: true, format: { with: /[\p{katakana} ー－]+/ }
   validates :first_furigana, presence: true, format: { with: /[\p{katakana} ー－]+/ }
   has_one :address
