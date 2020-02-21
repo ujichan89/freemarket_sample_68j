2aac4f6001a6391b11a6a82cb6cefbafabaf688c
ujichan89 <bbl.029.uji@gmail.com>
2020-02-21 14:58:56 +0900

    購入１

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,22 @@ Rails.application.routes.draw do
   resources :users
   resources :purchases
   resources :addresses
-  resources :credit_cards
+  resources :credit_cards,only:[:new, :destroy, :show]
 
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
+
+  resources :purchase, only: [:index] do
+    collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
+    end
+  end
 end
