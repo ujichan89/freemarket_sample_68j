fab05e4806776b9508cbff49c614f7fca72c6fc7
hanaharafumiki <59904899+hanaharafumiki@users.noreply.github.com>
2020-02-21 14:58:18 +0900

    Revert "[WIP]user登録"

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, :birthday_year, :birthday_month, :birthday_day, presence: true
-  validates :encrypted_password, presence: true, length: { minimum: 7 }
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-
-  validates :family_name, presence: true, format: { with: /\A(?:\p{Hiragana}|[ー－]|[一-龠々])+\z/ }
-  validates :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|[ー－]|[一-龠々])+\z/ }
-  validates :family_furigana, presence: true, format: { with: /[\p{katakana} ー－]+/ }
-  validates :first_furigana, presence: true, format: { with: /[\p{katakana} ー－]+/ }
-  has_one :address
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_035612) do
-
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "family_name"
-    t.string "first_name"
-    t.string "family_furigana"
-    t.string "first_furigana"
-    t.integer "number"
-    t.string "prefecture"
-    t.string "municipality"
-    t.string "street"
-    t.string "building"
-    t.integer "phone_number"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_02_18_034506) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -35,23 +18,14 @@ ActiveRecord::Schema.define(version: 2020_02_19_035612) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.string "family_furigana", null: false
-    t.string "first_furigana", null: false
-    t.integer "birthday_year", null: false
-    t.integer "birthday_month", null: false
-    t.integer "birthday_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
