541ef21aaf90e096a5eec259676a80b5473fa482
hanaharafumiki <hanahara@cornerprinting.com>
2020-02-24 18:55:47 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   root to: 'items#index'
 
   resources :users, except: [:index]
-  resources :items, only: [:index, :new, :show, :edit, :update, :destroy ]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :purchases
   resources :addresses
   resources :credit_cards
