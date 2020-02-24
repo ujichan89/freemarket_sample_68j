ef5036b34e3f414de82734478c4e8e90e5eff14b
reyon08 <sorairo.benimairoi@gmail.com>
2020-02-24 12:21:52 +0900

    [WIP]エラーハンドリング
    #what
    エラーハンドリングに関する記述を行いました
    主に、他の文字と重ならないよう、scssファイルを中心に行いました
    #wht
    エラーハンドリングを出すため

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_02_21_101400) do
     t.string "municipality"
     t.string "street"
     t.string "building"
-    t.string "phone_number"
+    t.integer "phone_number"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -55,6 +55,11 @@ ActiveRecord::Schema.define(version: 2020_02_21_101400) do
     t.index ["user_id"], name: "index_credit_cards_on_user_id"
   end
 
+  create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "picture"
     t.bigint "item_id"
@@ -64,22 +69,8 @@ ActiveRecord::Schema.define(version: 2020_02_21_101400) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.integer "price"
-    t.string "status"
-    t.text "description"
-    t.string "charge"
-    t.string "area"
-    t.string "day"
-    t.integer "sale"
-    t.bigint "user_id"
-    t.bigint "category_id"
-    t.bigint "brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["brand_id"], name: "index_items_on_brand_id"
-    t.index ["category_id"], name: "index_items_on_category_id"
-    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
