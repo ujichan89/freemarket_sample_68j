764766de413b2354a8e0e222480d1fe5750bffac
hanaharafumiki <hanahara@cornerprinting.com>
2020-02-24 11:22:07 +0900

    複数画像投稿機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
-
-  # belongs_to :user
-  belongs_to :category
+  # belongs_to user, foreign_key: 'user_id'
+  # belongs_to brand, foreign_key: 'brand_id'
+  # belongs_to category
+  has_many :images
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
