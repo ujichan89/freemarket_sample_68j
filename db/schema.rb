d2437bb500290abff8c56be141925dfb8ca995a1
hanaharafumiki <hanahara@cornerprinting.com>
2020-02-24 14:00:27 +0900

    deploy errorの修正

--- db/schema.rb
+++ db/schema.rb
@@ -55,11 +55,6 @@ ActiveRecord::Schema.define(version: 2020_02_21_101400) do
     t.index ["user_id"], name: "index_credit_cards_on_user_id"
   end
 
-  create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "picture"
     t.bigint "item_id"
