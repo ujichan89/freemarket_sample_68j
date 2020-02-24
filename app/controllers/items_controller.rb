a9dd9f0fdcf36fc4841094993e1df774fdddd6e6
reyon08 <sorairo.benimairoi@gmail.com>
2020-02-24 15:11:29 +0900

    [WIP]削除について
    #what
    コントローラー削除の記述を書きました
    #why
    コントローラー削除を作るため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,18 @@ class ItemsController < ApplicationController
   def show
 
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
+  def destory
+    item = Item.find(params[:id])
+    item.destory
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,10 @@ Rails.application.routes.draw do
   root to: 'items#index'
 
   resources :users, except: [:index]
-  resources :items
+  resources :items, only: [:index, :new, :show, :edit, :update, :destory ]
   resources :purchases
   resources :addresses
   resources :credit_cards
 
 end
+
