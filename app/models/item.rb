c95d522a99215883e546d5494da89faab0a0942c
hanaharafumiki <hanahara@cornerprinting.com>
2020-02-25 17:45:41 +0900

    itemモデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
   has_many :images
+  accepts_nested_attributes_for :images, allow_destroy: true
+  validates_associated :images
+  validates :images, presence: true
+  
   belongs_to :brand
   belongs_to :category
   belongs_to :user
-  accepts_nested_attributes_for :images, allow_destroy: true
 
-  validates_associated :images
-  validates :images, presence: true
 end
