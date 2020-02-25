cab46b1bd227e4e9cbf21096c0d9621e60c79535
kamui58 <owlprotectthewillage.58@gmail.com>
2020-02-25 18:45:10 +0900

    siderの修正

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,29 +1,29 @@
 document.addEventListener(
   "DOMContentLoaded", function(){
     if (document.getElementById("token_submit") != null) {
-      Payjp.setPublicKey("pk_test_b13ed2e277fedcfbf1f825bd");//ここに公開鍵を直書き
+      Payjp.setPublicKey("pk_test_b13ed2e277fedcfbf1f825bd");
       let btn = document.getElementById("token_submit");
-      btn.addEventListener("click", function(e) { //ボタンが押されたときに作動します
-        e.preventDefault(); //ボタンを一旦無効化します
+      btn.addEventListener("click", function(e) {
+        e.preventDefault();
         let card = {
           number: document.getElementById("card_number").value,
           cvc: document.getElementById("cvc").value,
           exp_month: document.getElementById("exp_month").value,
           exp_year: document.getElementById("exp_year").value
-        }; //入力されたデータを取得します。
+        };
         Payjp.createToken(card, (status, response) => {
-          if (status === 200) { //成功した場合
+          if (status === 200) {
             $("#card_number").removeAttr("name");
             $("#cvc").removeAttr("name");
             $("#exp_month").removeAttr("name");
-            $("#exp_year").removeAttr("name"); //データを自サーバにpostしないように削除
+            $("#exp_year").removeAttr("name");
             $("#card_token").append(
               $('<input type="hidden" name="payjp-token">').val(response.id)
-            ); //取得したトークンを送信できる状態にします
+            );
             document.inputForm.submit();
-            alert("登録が完了しました"); //確認用
+            alert("登録が完了しました");
           } else {
-            alert("カード情報が正しくありません。"); //確認用
+            alert("カード情報が正しくありません。");
           }
         });
       });
