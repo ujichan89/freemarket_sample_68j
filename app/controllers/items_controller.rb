0fe32470df79238254a55a1ce5468c1c945a4f8a
tabiojisann <takumiezumi@icloud.com>
2020-02-25 20:45:43 +0900

    商品出品入力フォーム

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
     @item.images.new
   end
  
-  def create
+  def create　
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path, notice: '出品できました'
