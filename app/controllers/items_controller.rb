99082e4efdc02244efb328318949d5ddb90f7cfc
NomuraIsao5656 <m011527326@edu.teu.ac.jp>
2020-02-25 14:39:39 +0900

    2/25_2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,11 @@
 class ItemsController < ApplicationController
   def index
-    @lankc = Item.group(:category_id).order('count_category_id DESC').count(:category_id)
-    lac    = @lankc.first
+    lac    = Item.group(:category_id).order('count_category_id DESC').count(:category_id).first
     category = lac[0]
     @cates = Item.where(sale: 0).where(category_id: category).first(3)
     @piccs = Image.where(item_id: @cates).distinct
 
-    @lankb = Item.group(:brand_id).order('count_brand_id DESC').count(:brand_id)
-    lab    = @lankb.first
+    lab    = Item.group(:brand_id).order('count_brand_id DESC').count(:brand_id).first
     brand  = lab[0]
     @bras  = Item.where(sale: 0).where(brand_id: brand).first(3)
     @picbs = Image.where(item_id: @bras).distinct
