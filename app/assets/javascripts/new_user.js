ba7ee84a66c17e0ab1edf5d8154b6c214ceaeaed
reyon08 <sorairo.benimairoi@gmail.com>
2020-02-26 14:33:49 +0900

    [WIP]user/addressエラーハンドリング
    #what
    user部分のjsファイル
    user / address の haml/scssファイルを編集しました
    #why
    user 登録のエラーハンドリングをjQuery型で記述したため

--- app/assets/javascripts/new_user.js
+++ app/assets/javascripts/new_user.js
@@ -40,7 +40,6 @@ function input_check(){
   $("#Input_error__birthday__select").empty();
 
 
-
   // 入力内容セット
   let nickname         =$('#nickname').val();
   let email            =$('#email').val();
@@ -51,8 +50,8 @@ function input_check(){
   let family_furigana  =$('#family_furigana').val();
   let first_furigana   =$('#first_furigana').val();
   let birthday_year    =$('.birthday_year').val();
-  // let birthday_month   =$('#birthday_month').val();
-  // let birthday_day     =$('#birthday_day').val();
+  let birthday_month   =$('#birthday_month').val();
+  let birthday_day     =$('#birthday_day').val();
   
 
 
@@ -126,20 +125,136 @@ function input_check(){
    result = false;
   }
 
-  //生年月日 日
+  //生年月日 年
    if(birthday_year == ""){
     $("#Input_error__birthday__select").html("生年月日を入力してください");
-    // $("#birthday_year").addClass("inp_error");
-    // $("#birthday_month").addClass("inp_error");
-    // $("#birthday_day").addClass("inp_error");
     result = false;
    }
 
-  //生年月日 日
-  if(birthday_year == ""){
+  //生年月日 月
+  if(birthday_month == ""){
     $("#Input_error__birthday__select").html("生年月日を入力してください");
     result = false;
    }
+
+     //生年月日 日
+  if(birthday_day == ""){
+    $("#Input_error__birthday__select").html("生年月日を入力してください");
+    result = false;
+   }
+
   return result;
 }
 
+
+//__________________________________________________________________________________________________
+
+
+以下アドレス
+
+新規登録時  例外処理
+
+$(function(){
+  $("#AddressInput__btn").on('click',function(e){
+    e.preventDefault();                      //エラー文消さない
+    if(input_check()){                        //
+      return false;                           //親要素のイベントを発生させない
+    }
+  });
+});
+
+// 入力内容チェックのための関数
+function input_check(){
+  let result = true;
+
+  // エラー用装飾のためのクラスリセット
+  $('#family_name_address').removeClass("inp_error");
+  $('#first_name_address').removeClass("inp_error");
+  $('#family_furigana_address').removeClass("inp_error");
+  $('#first_furigana_address').removeClass("inp_error");
+  $('#number').removeClass("inp_error");
+  $('#prefecture').removeClass("inp_error");
+  $('#municipality').removeClass("inp_error");
+  $('#street').removeClass("inp_error");
+
+
+  // 入力エラー文をリセット
+  $("#Input_error__family_name_address").empty();
+  $("#Input_error__first_name_address").empty();
+  $("#Input_error__family_furigana_address").empty();
+  $("#Input_error__first_furigana_address").empty();
+  $("#Input_error__number").empty();
+  $("#Input_error__prefecture").empty();
+  $("#Input_error__municipality").empty();
+  $("#Input_error__street").empty();
+
+  // 入力内容セット
+  let family_name_address           =$('#family_name_address').val();
+  let first_name_address            =$('#first_name_address').val();
+  let family_furigana_address       =$('#family_furigana_address').val();
+  let first_furigana_address        =$('#first_furigana_address').val();
+  let number                        =$('#number').val();
+  let address_prefecture            =$('#address_prefecture').val();
+  let municipality                  =$('#municipality').val();
+  let street                        =$('#street').val();
+
+  // 入力内容チェック
+
+  //苗字
+  if(family_name_address == ""){
+    $("#Input_error__family_name_address").html("苗字を入力してください");
+    $("#family_name_address").addClass("inp_error");
+    result = false;
+   }
+
+   //名前
+   if(first_name_address == ""){
+    $("#Input_error__first_name_address").html("名前を入力してください");
+    $("#family_name_address").addClass("inp_error");
+    result = false;
+   }
+ 
+   //苗字(カナ)
+   if(family_furigana_address == ""){
+    $("#Input_error__family_furigana_address").html("苗字カナを入力してください");
+    $("#family_furigana_address").addClass("inp_error");
+    result = false;
+   }
+ 
+   //苗字(カナ)
+   if(first_furigana_address == ""){
+    $("#Input_error__first_furigana_address").html("名前カナを入力してください");
+    $("#namemeikana_address").addClass("inp_error");
+    result = false;
+   }
+
+  // 郵便番号
+  if(number == ""){
+   $("#Input_error__number").html("郵便番号を入力してください");
+   $("#number").addClass("inp_error");
+   result = false;
+  }
+
+  //都道府県
+  if(address_prefecture == ""){
+   $("#Input_error__prefecture").html("都道府県を入力してください");
+   $("#address_prefecture").addClass("inp_error");
+   result = false;
+  }
+
+  //市町村区
+  if(municipality == ""){
+   $("#Input_error__municipality").html("市町村区を入力してください");
+   $("#municipality").addClass("inp_error");
+   result = false;
+  }
+
+  //番地
+  if(street == ""){
+   $("#Input_error__street").html("番地を入力してください");
+   $("#street").addClass("inp_error");
+   result = false;
+  }
+
+  return result;
+}
