a68437eb808545e4701b8702cf44d9fb1571c538
hanaharafumiki <hanahara@cornerprinting.com>
2020-03-02 18:54:08 +0900

    スライドショーの修正

--- app/assets/javascripts/bxslider.js
+++ app/assets/javascripts/bxslider.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
-  $('.slider').bxSlider({
-    slideHight: 100,
-    slideWidth: 500,
+$(function() {
+  // サムネイルのサイズ
+  var thumbWidth = 160;
+  var thumbHeight = 100;
+
+  // サムネイルの作成
+  var insert = '';
+  for (var i = 0; i < $('#sample li').length; i++) {
+      insert += '<a data-slide-index="' + i + '" href="#"><img src="' + $('#sample li').eq(i).children('img').attr('src') + '" width="' + thumbWidth + '" height="' + thumbHeight + '" /></a>';
+  };
+  $('.custom-thumb').append(insert);
+
+  $('#sample').bxSlider({
+      pagerCustom: '.custom-thumb',
   });
 });
