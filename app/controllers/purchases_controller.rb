c52dd79862d14833632fb64546c84672e1fa15ec
ujichan89 <bbl.029.uji@gmail.com>
2020-03-03 12:28:10 +0900

    購入ボタンの表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,15 +3,23 @@ class PurchasesController < ApplicationController
   require 'payjp'
 
   def edit
-    @item = set_item
-    card = set_card
-    if card.blank?
-      redirect_to controller: "cards", action: "new"
-    else
-      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
-    end
+    # ////売り切れてたら別のページに行けるようにする
+    # ①いふ文で売り切れてないのであればコメントアウトの処理をする。
+    # ②売り切れているならエルスでリダイレクトとかで別のページに飛ばす。
+    # Item.find(params[:id])
+    # if Item.sale == 0
+      @item = set_item
+      card = set_card
+      if card.blank?
+        redirect_to controller: "cards", action: "new"
+      else
+        Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
+        customer = Payjp::Customer.retrieve(card.customer_id)
+        @default_card_information = customer.cards.retrieve(card.card_id)
+      end
+    # else
+    #   redirect_to controllre: "users", action: "show"
+    # end
   end
 
   def pay
